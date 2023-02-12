4a28fa2164c31275bb3c3badbecdabff03b8b282
Vladimir Mosunov <vladimir_mosunov@mail.ru>
2023-02-12 15:06:13 +0300

    fix main

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import vk_api
 from random import randrange
-# import requests
 from vk_api.longpoll import VkLongPoll, VkEventType
-# from bs4 import BeautifulSoup
-from vk_bot import Vkbot
+from vk_bot import Vk_user
 from vk_api.keyboard import VkKeyboard, VkKeyboardColor
-import json
+from bd import UsersBd
+import random
 
 def write_msg(user_id, message=None, keyboard=None, attachment=None):
     post = {'user_id': user_id, 'message': message,  'random_id': randrange(10 ** 7)}
@@ -16,68 +15,80 @@ def write_msg(user_id, message=None, keyboard=None, attachment=None):
 
     vk.method('messages.send', post)
 
+def key_b(keyb):
+
+    keyb.add_button(label='Выйти', color=VkKeyboardColor.SECONDARY)
+    keyb.add_line()
+    keyb.add_button(label='Следующая', color=VkKeyboardColor.SECONDARY)
+    keyb.add_line()
+    keyb.add_button(label='Добавить в избранное', color=VkKeyboardColor.SECONDARY)
+    keyb.add_line()
+    keyb.add_button(label='Мои избранные', color=VkKeyboardColor.SECONDARY)
+
+
 with open(r'C:\Users\vladi\Desktop\project\key_api.txt', 'r') as f:
     token = f.read()
 
-with open(r'C:\Users\vladi\Desktop\project\people.json', 'r', encoding='utf-8') as p:
-    peoples = json.load(p)
 
+token_1 = "...." # Токен админа чат бота 
 
+settings_1 = dict(one_time=False, inline=True)
+settings = dict(one_time=True, inline=False)
 
-settings = dict(one_time=False, inline=True)
-settings_1 = dict(one_time=True, inline=False)
 if __name__ == '__main__':
 
     vk = vk_api.VkApi(token=token)
-    
+    vk_1 = vk_api.VkApi(token=token_1)
     longpoll = VkLongPoll(vk)
     flag = True
+    flag_1 = True
 
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW:
             if event.to_me:
-                text = event.text.lower()
-                print('New message:')
-                print(f'For me by: {event.user_id}', end=' ')
+                while flag_1:
+                    bot = Vk_user(event.user_id, vk_1, token_1)
+                    flag_1 = False
 
-                
-                bot = Vkbot(event.user_id)
-                keyboard = VkKeyboard(**settings_1)
-                keyboard_1 = VkKeyboard(**settings)
+                keyboard = VkKeyboard(**settings)
+                keyboard_1 = VkKeyboard(**settings_1)
                 while flag:
                     keyboard.add_button(label='Начать', color=VkKeyboardColor.SECONDARY)
-                    write_msg(event.user_id,'Для входа в приложение нажмите Начать', keyboard)
+                    write_msg(event.user_id,'Для входа в приложение нажмите Начать\n"При нажатии придется подождать пока прозойдет подборка пользователей по вашим данным"', keyboard)
+                    user = UsersBd()
+                    if user.find_user_person(event.user_id) == False:
+                        if bot.get_users_date() == 'Ошибка в дате рождения! Исправь!':
+                            break
+                        else:
+                            user.add_user(bot.get_users_date())
+
                     flag = False
                 
                 if event.text.upper() == 'НАЧАТЬ':
-                    keyboard_1.add_button(label='Выйти', color=VkKeyboardColor.SECONDARY)
-                    keyboard_1.add_line()
-                    keyboard_1.add_button(label='Следующая', color=VkKeyboardColor.SECONDARY)
-                    keyboard_1.add_line()
-                    keyboard_1.add_button(label='Добавить в избранное', color=VkKeyboardColor.SECONDARY)
-                    write_msg(event.user_id,f'{peoples[0]["first_name"]} {peoples[0]["last_name"]}', keyboard_1, attachment=peoples[0]["photo"])
-                    del peoples[0]
+                    if bot.main() == 'Ошибка в дате рождения! Исправь!':
+                        keyboard_1.add_button(label='Выйти', color=VkKeyboardColor.SECONDARY)
+                        keyboard_1.add_line()
+                        write_msg(event.user_id,'Я не смог найти твою дату рождения или город! Проверь свои данные в настройках', keyboard)
+                    else:
+                        bot.main
+                        key_b(keyboard_1)
+                        rand = random.randrange(len(bot.list_users))
+                        write_msg(event.user_id,f'{bot.list_users[rand]["first_name"]} {bot.list_users[rand]["last_name"]}', keyboard_1, attachment=bot.list_users[rand]["photo"])
+                        del bot.list_users[rand]
                 elif event.text.upper() == 'ВЫЙТИ':
                     keyboard.add_button(label='Начать', color=VkKeyboardColor.SECONDARY)
                     write_msg(event.user_id,'Для входа в приложение нажмите Начать', keyboard)
                 elif event.text.upper() == 'СЛЕДУЮЩАЯ':
+                    key_b(keyboard_1)
+                    rand = random.randrange(len(bot.list_users))
+                    write_msg(event.user_id,f'{bot.list_users[rand]["first_name"]} {bot.list_users[rand]["last_name"]}', keyboard_1, attachment=bot.list_users[rand]["photo"])
+                    del bot.list_users[rand]
+                elif event.text.upper() == 'ДОБАВИТЬ В ИЗБРАННОЕ':
+                    key_b(keyboard_1) 
+                    rand = random.randrange(len(bot.list_users))
+                    user.add_favorites(str(event.user_id), bot.list_users[rand])
+                    write_msg(event.user_id,f'{bot.list_users[rand]["first_name"]} {bot.list_users[rand]["last_name"]}', keyboard_1, attachment=bot.list_users[rand]["photo"])
+                    del bot.list_users[rand]
+                elif event.text.upper() == 'МОИ ИЗБРАННЫЕ':
                     keyboard_1.add_button(label='Выйти', color=VkKeyboardColor.SECONDARY)
-                    keyboard_1.add_line()
-                    keyboard_1.add_button(label='Следующая', color=VkKeyboardColor.SECONDARY)
-                    keyboard_1.add_line()
-                    keyboard_1.add_button(label='Добавить в избранное', color=VkKeyboardColor.SECONDARY)
-                    write_msg(event.user_id,f'{peoples[0]["first_name"]} {peoples[0]["last_name"]}', keyboard_1, attachment=peoples[0]["photo"])
-                    del peoples[0]
-
-                    # keyboard_1.add_line()
-                    # keyboard_1.add_button(label='Следующая', color=VkKeyboardColor.SECONDARY)
-                    # keyboard_1.add_line()
-                    # keyboard_1.add_button(label='Добавить в избранное', color=VkKeyboardColor.SECONDARY)
-                    # write_msg(event.user_id,'Привет', keyboard)
-                # else:
-                #     write_msg(event.user_id, bot.new_message(event.text), keyboard)
-
-                
-                print('Text: ', event.text)
-                # ['photo21263797_456240866', 'photo21263797_456239150']
-        
+                    write_msg(event.user_id,user.find_all_favorites(str(event.user_id)), keyboard_1)
